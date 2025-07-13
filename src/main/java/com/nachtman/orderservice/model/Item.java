58c28eb37e50690ab8ac11be66af9e298d219175
Filip Nachtman <nachtman.filip@seznam.cz>
2025-07-13 16:01:10 +0200

    added id and collection for mongodb func

--- src/main/java/com/nachtman/orderservice/model/Item.java
+++ src/main/java/com/nachtman/orderservice/model/Item.java
@@ -1,5 +1,8 @@
 package com.nachtman.orderservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-public record Item(String item_id, String description, Double price) {
+@Document(collection = "items")
+public record Item(@Id String item_id, String description, Double price) {
 }
--- src/main/java/com/nachtman/orderservice/model/Order.java
+++ src/main/java/com/nachtman/orderservice/model/Order.java
@@ -1,8 +1,12 @@
 package com.nachtman.orderservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.Instant;
 import java.util.List;
 
-public record Order(String order_id, String account_id, String costumer_name, String order_status, List<Item> items,
+@Document(collection = "orders")
+public record Order(@Id String order_id, String account_id, String costumer_name, String order_status, List<Item> items,
                     Instant created_at, Instant updated_at) {
 }
